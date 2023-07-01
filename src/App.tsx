75e43f30aa354a932b2ed7cd9dbdcde47c006c91
Chen Jie <61531625+eijnehc@users.noreply.github.com>
2023-07-01 18:28:48 +0800

    Zod Integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { YoutubeForm } from "./components/YoutubeForm";
 import { YupYouTubeForm } from "./components/YupYoutubeForm";
+import { ZodYouTubeForm } from "./components/ZodYouTubeForm";
 
 function App() {
   return (
     <div>
-      <YupYouTubeForm />
+      <ZodYouTubeForm />
     </div>
   );
 }
